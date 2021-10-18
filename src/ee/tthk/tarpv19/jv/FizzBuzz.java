8572b9b2da3898c11cf71b66cbb98df202595be3
Opilane TTHK <opilane@tthk.ee>
2021-10-18 09:28:29 +0300

    For numbers which are multiples of both three and five print FizzBuzz

--- src/ee/tthk/tarpv19/jv/FizzBuzz.java
+++ src/ee/tthk/tarpv19/jv/FizzBuzz.java
@@ -1,11 +1,23 @@
 package ee.tthk.tarpv19.jv;
 
 public class FizzBuzz {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++){
+			System.out.println(of(i));
+		}
+	}
 
 
 	public Object getNumbers() {
 		// TODO Auto-generated method stub
 		return new int[100];
+	}
+	public static String of(int i) {
+		if(i == 0) return "0";
+		if(i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
+		if(i % 3 == 0) return "Fizz";
+		if(i % 5 == 0) return "Buzz";
+		return String.valueOf(i);
 	}
 
 }
